8502f1c4831586959043fae125382e7f1d27211b
Matheus M <matheus150101miranda@gmail.com>
2023-01-10 15:04:34 -0300

    Feat(mocks): add mocks session and author / interfaces author and session

--- src/interfaces/author.interface.ts
+++ src/interfaces/author.interface.ts
@@ -1,2 +1,19 @@
+interface ICreateUserRequest {
+  name: string;
+  email: string;
+  password: string;
+  city: string;
+  country: string;
+  isAdmin?: boolean;
+}
 
-export {};
+interface IOmitUserPassword extends Omit<ICreateUserRequest, "password"> {}
+
+interface ICreateUserResponse extends IOmitUserPassword {
+  id: number;
+  isAdmin: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export { ICreateUserRequest, IOmitUserPassword, ICreateUserResponse };
