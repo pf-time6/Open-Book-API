d7a5dd5cab31a66711fcc54c8d10d063beeaa757
Igorttdp <igorttdp.dev@email.com>
2023-01-10 10:21:17 -0300

    feat: add missing structure files

---
+++ src/routes/author.routes.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const authorRoutes = Router();
+
+export default authorRoutes
---
+++ src/routes/books.routes.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const booksRoutes = Router();
+
+export default booksRoutes;
