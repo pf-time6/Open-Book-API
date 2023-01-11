e15a6292a6dc735eddf0e574b42fa399f1ac2523
daniel delos silva <apeiron52@gmail.com>
2023-01-11 10:36:26 -0300

    feat: post categories

--- src/services/categories/createCategories.service.ts
+++ src/services/categories/createCategories.service.ts
@@ -2,11 +2,11 @@ import AppDataSource from "../../data-source";
 import Categories from "../../entities/categories.entity";
 import { ICategoryRequest } from "../../interfaces/categories.interface";
 
-const createCategoryService = async (categoryData: ICategoryRequest) =>{
-const categoryRepo = AppDataSource.getRepository(Categories)
-const createdCategory = categoryRepo.create(categoryData)
-await categoryRepo.save(createdCategory)
-return createdCategory
-}
+const createCategoryService = async (categoryData: ICategoryRequest) => {
+  const categoryRepo = AppDataSource.getRepository(Categories);
+  const createdCategory = categoryRepo.create(categoryData);
+  await categoryRepo.save(createdCategory);
+  return createdCategory;
+};
 
-export default createCategoryService
+export default createCategoryService;
