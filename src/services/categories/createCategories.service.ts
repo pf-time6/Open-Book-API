ac0a7dd05e61cc0341759722c178af39be25f539
daniel delos silva <apeiron52@gmail.com>
2023-01-11 10:25:19 -0300

    feat: route/categories_daniel post de categories

---
+++ src/services/categories/createCategories.service.ts
@@ -0,0 +1,12 @@
+import AppDataSource from "../../data-source";
+import Categories from "../../entities/categories.entity";
+import { ICategoryRequest } from "../../interfaces/categories.interface";
+
+const createCategoryService = async (categoryData: ICategoryRequest) =>{
+const categoryRepo = AppDataSource.getRepository(Categories)
+const createdCategory = categoryRepo.create(categoryData)
+await categoryRepo.save(createdCategory)
+return createdCategory
+}
+
+export default createCategoryService
