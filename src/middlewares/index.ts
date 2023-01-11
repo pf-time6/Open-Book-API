aaafec95923cb084b4ebba3a5a259163b2991a56
daniel delos silva <apeiron52@gmail.com>
2023-01-11 12:18:39 -0300

    feat: middlewareIsadm and verify alreadyExistCategory

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,4 +1,7 @@
 import validateSchemaMiddleware from "./validateSchema.middleware";
 import ensureAuthMiddleware from "./ensureAuth.middleware";
+import ensureAuthorAdmMiddleware from "./ensureAuthorAdm.middleware";
 
-export { validateSchemaMiddleware, ensureAuthMiddleware };
+export { validateSchemaMiddleware, ensureAuthMiddleware, ensureAuthorAdmMiddleware};
+
+
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { createCategoryController } from "../controllers/categories";
+import { ensureAuthMiddleware, ensureAuthorAdmMiddleware } from "../middlewares";
 
 const categoriesRoutes = Router();
 
-categoriesRoutes.post('', createCategoryController)
+categoriesRoutes.post('',ensureAuthMiddleware, ensureAuthorAdmMiddleware, createCategoryController)
 
 export default categoriesRoutes;
