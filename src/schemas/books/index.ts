fe0302c9103df12575eb66f58b23b187ca4002c0
Marcelo Henrique Aguiar Marques <96503481+MHAMarques@users.noreply.github.com>
2023-01-12 11:35:48 -0300

    feat: Route Book - Get Books ID
    Rota de GET /books/:id para mostrar um livro específico.

--- src/schemas/books/index.ts
+++ src/schemas/books/index.ts
@@ -1,6 +1,7 @@
 // Centralizar imports
 import createBooksRequestSchema from "./createBookRequest.schema";
 import listBooksResponseSchema from "./listBooksResponse.schema";
+import showBookResponseSchema from "./showBookResponse.schema";
 
-export { createBooksRequestSchema, listBooksResponseSchema };
+export { createBooksRequestSchema, listBooksResponseSchema, showBookResponseSchema };
 
---
+++ src/schemas/books/showBookResponse.schema.ts
@@ -0,0 +1,20 @@
+import * as yup from "yup";
+
+const showBookResponseSchema = yup.object().shape({
+    author: yup
+      .object({
+        country: yup.string().notRequired(),
+        city: yup.string().notRequired(),
+        name: yup.string().notRequired(),
+        email: yup.string().notRequired(),
+        id: yup.string().notRequired(),
+      })
+      .notRequired(),
+    coverUrl: yup.string().notRequired(),
+    category: yup.array(yup.number()).notRequired(),
+    about: yup.string().notRequired(),
+    title: yup.string().notRequired(),
+    id: yup.string().notRequired(),
+  });
+
+export default showBookResponseSchema;
