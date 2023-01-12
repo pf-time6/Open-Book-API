af4d4e5bfe00ff1f4859b8c20bf860e8de630ee3
= <cadastro@diegogaspar.dev.br>
2023-01-12 13:57:15 -0300

    Feat: middleware Email

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,5 +2,12 @@ import validateSchemaMiddleware from "./validateSchema.middleware";
 import ensureAuthMiddleware from "./ensureAuth.middleware";
 import ensureAlreadyExistChapter from "./ensureAlreadyExistChapter.middleware";
 import ensureBookExists from "./ensureBookExists.middleware";
+import ensureEmailExistsMiddleware from "./ensureEmailExists.middleware";
 
-export { validateSchemaMiddleware, ensureAuthMiddleware, ensureAlreadyExistChapter, ensureBookExists };
+export {
+  validateSchemaMiddleware,
+  ensureAuthMiddleware,
+  ensureAlreadyExistChapter,
+  ensureBookExists,
+  ensureEmailExistsMiddleware,
+};
