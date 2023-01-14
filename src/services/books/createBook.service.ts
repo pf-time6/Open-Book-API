e89523dd56c8e662cbe82dab4f8c212237e58778
Matheus M <matheus150101miranda@gmail.com>
2023-01-14 08:56:30 -0300

    retirando try/catch do createbooks.service

--- src/services/books/createBook.service.ts
+++ src/services/books/createBook.service.ts
@@ -8,70 +8,65 @@ import { ICreateBookRequest } from "../../interfaces/books.interface";
 import { Any, In } from "typeorm";
 
 const createBookService = async (body: ICreateBookRequest, userId: string) => {
-  console.log("oi");
-  try {
-    const booksRepo = AppDataSource.getRepository(Books);
-    const bookFound = await booksRepo.findOne({
-      where: {
-        title: body.title,
-      },
-    });
+  const booksRepo = AppDataSource.getRepository(Books);
+  const bookFound = await booksRepo.findOne({
+    where: {
+      title: body.title,
+    },
+  });
 
-    if (bookFound) {
-      throw new AppError("Tittle already exists", 409);
-    }
-
-    const authorRepo = AppDataSource.getRepository(Author);
-    const authorFound = await authorRepo.findOne({
-      where: {
-        id: userId,
-      },
-    });
-
-    if (!authorFound) {
-      throw new AppError("Author not found", 409);
-    }
-
-    const books = booksRepo.create({
-      ...body,
-      author: authorFound,
-    });
-
-    const categoriesRepo = AppDataSource.getRepository(Categories);
-    const bcRepo = AppDataSource.getRepository(Books_Categories);
-
-    const loopCategories = await categoriesRepo.findBy({
-      id: In([...body.category]),
-    });
-
-    if (loopCategories.length !== body.category.length) {
-      throw new AppError(
-        `There is at least one category that is not in the database.`,
-        404
-      );
-    }
-
-    await booksRepo.save(books);
-
-    body.category.map(async (el) => {
-      const categories = await categoriesRepo.findOneBy({
-        id: el,
-      });
-
-      if (!categories) {
-        return;
-      }
-
-      const books_categories = bcRepo.create({
-        books,
-        categories,
-      });
-      await bcRepo.save(books_categories);
-    });
-    return books;
-  } catch (error) {
-    console.log(error);
+  if (bookFound) {
+    throw new AppError("Tittle already exists", 409);
   }
+
+  const authorRepo = AppDataSource.getRepository(Author);
+  const authorFound = await authorRepo.findOne({
+    where: {
+      id: userId,
+    },
+  });
+
+  if (!authorFound) {
+    throw new AppError("Author not found", 409);
+  }
+
+  const books = booksRepo.create({
+    ...body,
+    author: authorFound,
+  });
+
+  const categoriesRepo = AppDataSource.getRepository(Categories);
+  const bcRepo = AppDataSource.getRepository(Books_Categories);
+
+  const loopCategories = await categoriesRepo.findBy({
+    id: In([...body.category]),
+  });
+
+  if (loopCategories.length !== body.category.length) {
+    throw new AppError(
+      `There is at least one category that is not in the database.`,
+      404
+    );
+  }
+
+  await booksRepo.save(books);
+
+  body.category.map(async (el) => {
+    const categories = await categoriesRepo.findOneBy({
+      id: el,
+    });
+
+    if (!categories) {
+      return;
+    }
+
+    const books_categories = bcRepo.create({
+      books,
+      categories,
+    });
+    await bcRepo.save(books_categories);
+  });
+  return books;
 };
 
 export default createBookService;
