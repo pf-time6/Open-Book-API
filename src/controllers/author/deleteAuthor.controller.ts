97e31963d33732a60e79d452e73d6f5dfd80c805
pabloaraujo <pabloaraujo_engmec@hotmail.com>
2023-01-17 09:05:29 -0300

    route(delete): status 202 change

--- src/controllers/author/deleteAuthor.controller.ts
+++ src/controllers/author/deleteAuthor.controller.ts
@@ -4,7 +4,7 @@ import { deleteAuthorService } from "../../services/author";
 const deleteAuthorController = async (req: Request, res: Response) => {
   const data = await deleteAuthorService(req.params.id);
 
-  return res.status(201).json(data);
+  return res.status(202).json(data);
 };
 
 export default deleteAuthorController;
