87c48040e9d72ccc47ca5e2eb658e87a012d2b2d
Igorttdp <igorttdp.dev@email.com>
2023-01-17 12:30:05 -0300

    feat: add final tests for delete feature

--- src/__tests__/integration/author/deleteAuthor.spec.ts
+++ src/__tests__/integration/author/deleteAuthor.spec.ts
@@ -29,17 +29,18 @@ describe("Delete Author tests", () => {
   });
 
   beforeEach(async () => {
-    const authors = await authorRepo.find();
+    const authors = await authorRepo.find({
+      withDeleted: true,
+    });
+
     await authorRepo.remove(authors);
   });
 
-  it("DELETE: /author/:id -> Should be able to delete any author account with adm permission", async () => {
+  it("DELETE: /author/:id -> Should be able to soft delete any author account with adm permission", async () => {
     const authorPayload = mockedCommonAuthorRequest;
     const admAuthorPayload = mockedAdminAuthorRequest;
 
     const author = await request(app).post(baseUrl).send(authorPayload);
-
-    console.log(author.body);
 
     expect(author.status).toBe(201);
     expect(author.body).toHaveProperty("id");
@@ -109,5 +110,57 @@ describe("Delete Author tests", () => {
     const authorsList = await request(app).get(baseUrl);
 
     expect(authorsList.body).toHaveLength(1);
+  });
+
+  it("DELETE: /author/:id -> Should not be able to delete an Author with invalid id", async () => {
+    const admAuthorPayload = mockedAdminAuthorRequest;
+    const adminAuthor = await request(app).post(baseUrl).send(admAuthorPayload);
+
+    expect(adminAuthor.status).toBe(201);
+    expect(adminAuthor.body).toHaveProperty("id");
+
+    const loginDataAdmin = mockedAdminAuthorSession.sessionPayload;
+    const loginAdmin = await request(app).post("/login").send(loginDataAdmin);
+
+    expect(loginAdmin.status).toBe(200);
+    expect(loginAdmin.body).toHaveProperty("token");
+
+    const token: string = loginAdmin.body.token;
+
+    const response = await request(app)
+      .delete(`${baseUrl}/ff9421db-666a-4ba9-99e4-33f2d0ed13fd`)
+      .set("Authorization", `Bearer ${token}`);
+
+    const expectResults = {
+      status: 404,
+      bodyToHaveProperty: "message",
+    };
+
+    expect(response.status).toBe(expectResults.status);
+    expect(response.body).toHaveProperty(expectResults.bodyToHaveProperty);
+  });
+
+  it("DELETE: /author/:id -> Should not be able to delete an Author without token", async () => {
+    const admAuthorPayload = mockedAdminAuthorRequest;
+    const adminAuthor = await request(app).post(baseUrl).send(admAuthorPayload);
+
+    expect(adminAuthor.status).toBe(201);
+    expect(adminAuthor.body).toHaveProperty("id");
+
+    const loginDataAdmin = mockedAdminAuthorSession.sessionPayload;
+    const loginAdmin = await request(app).post("/login").send(loginDataAdmin);
+
+    expect(loginAdmin.status).toBe(200);
+    expect(loginAdmin.body).toHaveProperty("token");
+
+    const response = await request(app).delete(`${baseUrl}/ff9421db-666a-4ba9-99e4-33f2d0ed13fd`);
+
+    const expectResults = {
+      status: 401,
+      bodyToHaveProperty: "message",
+    };
+
+    expect(response.status).toBe(expectResults.status);
+    expect(response.body).toHaveProperty(expectResults.bodyToHaveProperty);
   });
 });
